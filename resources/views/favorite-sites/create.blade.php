4dcaa2b62bcbcf36a070067b462a4a84ac18224d
na7esan <na7esan@gmail.com>
2021-08-15 18:39:20 +0900

    お気に入りサイトの登録ページの作成

--- resources/views/favorite-sites/create.blade.php
+++ resources/views/favorite-sites/create.blade.php
@@ -1,1 +1,28 @@
-ここのお気に入りサイト登録画面を作成する
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>お気に入りサイト登録画面</title>
+</head>
+
+<body>
+    <h1>お気に入りサイト登録ページ</h1>
+
+    <form action="#" method="post">
+        @csrf
+
+        <label>サイト名
+            <input type="text" name="name">
+        </label>
+
+        <label>URL
+            <input type="text" name="url">
+        </label>
+
+        <button type="submit">登録</button>
+    </form>
+
+</body>
+
+</html>
