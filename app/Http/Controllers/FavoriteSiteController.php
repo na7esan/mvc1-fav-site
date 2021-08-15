fdbec9b694b7984ca6533a4ed7ab20bcc43b3660
na7esan <na7esan@gmail.com>
2021-08-15 18:23:16 +0900

    お気に入りサイト登録画面表示のためにルーティング作成

--- app/Http/Controllers/FavoriteSiteController.php
+++ app/Http/Controllers/FavoriteSiteController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class FavoriteSiteController extends Controller
 {
-    //
+    /**
+     * お気に入りサイトの登録画面表示処理
+     */
+    public function create()
+    {
+        return 'お気に入りサイトの登録画面';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
 
+use App\Http\Controllers\FavoriteSiteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('init');
-});
+Route::get('favorite-sites/create', [FavoriteSiteController::class, 'create']);
