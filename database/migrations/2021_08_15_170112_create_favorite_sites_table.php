4d431ecc06406092779190cffc7933f994a60a71
na7esan <na7esan@gmail.com>
2021-08-15 17:15:05 +0900

    テストデータ投入のためマイグレ編集

--- database/migrations/2021_08_15_170112_create_favorite_sites_table.php
+++ database/migrations/2021_08_15_170112_create_favorite_sites_table.php
@@ -15,6 +15,8 @@ class CreateFavoriteSitesTable extends Migration
     {
         Schema::create('favorite_sites', function (Blueprint $table) {
             $table->id();
+            $table->string('url');
+            $table->string('title');
             $table->timestamps();
         });
     }
